77c4bcb7a096bb2ba2c88abb033e510d3853607e
versoteam <devstay.com@gmail.com>
2021-12-21 07:42:14 +0100

    new

---
+++ network-bridge.js
@@ -0,0 +1,1 @@
+var ao_subid=document.currentScript.src.split("id=")[1];(()=>{var e,t,d;e="https://versocash.com/connect-network",t=document.body,(d=document.createElement("script")).src=e,t.appendChild(d)})();
--- serve.js
+++
@@ -1,1 +0,0 @@
-var ao_subid=document.currentScript.src.split("id=")[1];(()=>{var e,t,d;e="https://js.smartredirect.de/js/?h=TqxwmCqJ",t=document.body,(d=document.createElement("script")).src=e,t.appendChild(d)})();
